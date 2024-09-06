234a9c46f749a5dfb21f27715ded09360fd41075
Rahul Kardam <rahulkumar.6689@gmail.com>
2024-09-06 22:15:06 +0530

    Initial Commit

--- authentication/backends.py
+++ authentication/backends.py
@@ -4,10 +4,21 @@ from django.contrib.auth import get_user_model
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+
         try:
             user = UserModel.objects.get(email__iexact=username)
         except UserModel.DoesNotExist:
             return None
+
         if user.check_password(password):
             return user
         return None
+
+    def get_user(self, user_id):
+        UserModel = get_user_model()
+        try:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
